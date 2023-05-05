e9e124494d3e622dddaa44e08fe336324c2896ee
vpetrovaa <vpetrova.laba@solvd.com>
2023-05-05 19:38:04 +0300

    Project structure was generated

---
+++ src/main/java/com/solvd/elasticsearchmicroservice/RepositoryInitializer.java
@@ -0,0 +1,13 @@
+package com.solvd.elasticsearchmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryInitializer.class, args);
+	}
+
+}
