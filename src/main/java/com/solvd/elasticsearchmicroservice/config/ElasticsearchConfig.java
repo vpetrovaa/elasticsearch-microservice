41d4149e8ae36fb0e1b5c6bc14a70d93c8c1a9c3
vpetrovaa <vpetrova.laba@solvd.com>
2023-05-12 18:44:50 +0300

    Host was moved to application.yml

--- src/main/java/com/solvd/elasticsearchmicroservice/config/ElasticsearchConfig.java
+++ src/main/java/com/solvd/elasticsearchmicroservice/config/ElasticsearchConfig.java
@@ -6,16 +6,20 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticsearchConfig{
 
+    @Value("${elastic.host}")
+    private String elasticHost;
+
     @Bean
     public RestClient getRestClient() {
         RestClient restClient = RestClient.builder(
-                new HttpHost("localhost", 9200)).build();
+                new HttpHost(elasticHost, 9200)).build();
         return restClient;
     }
 
