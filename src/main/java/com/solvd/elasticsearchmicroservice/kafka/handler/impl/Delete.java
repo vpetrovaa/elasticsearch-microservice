6e181c7f8c2d56295c557e86d5cc8b6f3a87ed84
vpetrovaa <vpetrova.laba@solvd.com>
2023-05-19 11:09:51 +0300

    Handlers were renamed

--- src/main/java/com/solvd/elasticsearchmicroservice/kafka/handler/impl/Delete.java
+++ src/main/java/com/solvd/elasticsearchmicroservice/kafka/handler/impl/Delete.java
@@ -1,27 +1,20 @@
 package com.solvd.elasticsearchmicroservice.kafka.handler.impl;
 
-import com.solvd.elasticsearchmicroservice.domain.Note;
 import com.solvd.elasticsearchmicroservice.kafka.event.NoteEvent;
 import com.solvd.elasticsearchmicroservice.kafka.handler.Handler;
 import com.solvd.elasticsearchmicroservice.service.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@Component("PUT")
+@Component("DELETE")
 @RequiredArgsConstructor
-public class PutHandlerImpl implements Handler {
+public class Delete implements Handler {
 
     private final NoteService noteService;
 
     @Override
     public void handle(NoteEvent event) {
-        Note note = new Note();
-        note.setId(event.getId());
-        note.setTag(event.getTag());
-        note.setDescription(event.getDescription());
-        note.setTheme(event.getTheme());
-        note.setUserId(event.getUserId());
-        noteService.update(note);
+        noteService.delete(event.getId());
     }
 
 }
