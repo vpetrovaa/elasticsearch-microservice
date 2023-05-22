a16790e32214372ed6016b940a9fa59f66e919ed
vpetrovaa <vpetrova.laba@solvd.com>
2023-05-22 12:51:46 +0300

    Bean configuration of map was removed

--- src/main/java/com/solvd/elasticsearchmicroservice/kafka/KfConsumerImpl.java
+++ src/main/java/com/solvd/elasticsearchmicroservice/kafka/KfConsumerImpl.java
@@ -4,8 +4,6 @@ import com.solvd.elasticsearchmicroservice.kafka.event.NoteEvent;
 import com.solvd.elasticsearchmicroservice.kafka.handler.Handler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -16,17 +14,12 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class KfConsumerImpl implements KfConsumer{
 
-    private final ApplicationContext context;
-
-    @Bean
-    public Map<String, Handler> handlers(ApplicationContext context) {
-        return context.getBeansOfType(Handler.class);
-    }
+    private final Map<String, Handler> handlers;
 
     @KafkaListener(topics = "${kafka.topic}", groupId = "groupId", containerFactory = "kafkaListenerContainerFactory")
     public void receive(NoteEvent event) {
         log.info("Message received -> {}", event);
-        handlers(context).get(event.getType().name()).handle(event);
+        handlers.get(event.getType().name()).handle(event);
     }
 
 }
