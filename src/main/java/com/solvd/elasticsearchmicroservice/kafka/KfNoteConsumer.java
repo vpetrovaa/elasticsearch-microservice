1aa247b2fc42bfadd7d7a9485197192aab35771a
vpetrovaa <vpetrova.laba@solvd.com>
2023-05-22 18:28:25 +0300

    Consumer implementation was renamed

--- src/main/java/com/solvd/elasticsearchmicroservice/kafka/KfNoteConsumer.java
+++ src/main/java/com/solvd/elasticsearchmicroservice/kafka/KfNoteConsumer.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class KfConsumerImpl implements KfConsumer<NoteEvent> {
+public class KfNoteConsumer implements KfConsumer<NoteEvent> {
 
     private final Map<String, Handler> handlers;
 
